a381c1ebbc315a167917f13c1edcc1fefffc24ab
Mykyta Topchyi <mtopchyi@tripletwice.com>
2026-01-29 23:03:00 +0200

    implemet view installer for zenject

---
+++ Runtime/External/ZenjectBinder/RuntimeViewBinder.cs
@@ -0,0 +1,28 @@
+﻿#if EXTENJECT_SUPPORT
+
+using Mvvm.Factory;
+using UnityEngine;
+using Zenject;
+
+namespace Mvvm.External.ZenjectBinder
+{
+    public class RuntimeViewBinder : MonoBehaviour
+    {
+        [SerializeField] private View.View view;
+
+        private IViewBinder _binder;
+        
+        [Inject]
+        private void Construct(IViewBinder binder)
+        {
+            _binder = binder;
+        }
+
+        private void Start()
+        {
+            _binder.BindView(view);
+        }
+    }
+}
+
+#endif
---
+++ Runtime/External/ZenjectBinder/ViewInstaller.cs
@@ -0,0 +1,21 @@
+﻿#if EXTENJECT_SUPPORT
+
+using Mvvm.Binding;
+using Mvvm.Factory;
+using Zenject;
+
+namespace Mvvm.External.ZenjectBinder
+{
+    public class ViewInstaller : MonoInstaller
+    {
+        public override void InstallBindings()
+        {
+            Container.BindInterfacesTo<ContainerResolver>().AsSingle();
+            
+            Container.Bind<InstanceBinder>().AsTransient();
+            Container.BindInterfacesTo<InjectedViewBinder>().AsTransient();
+        }
+    }
+}
+
+#endif
